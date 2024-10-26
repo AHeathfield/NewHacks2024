5f636c082eb38c0ab05ac07dfb2576809ec6e994
MuizHamzat <muizhamzat@gmail.com>
2024-10-26 16:38:50 -0400

    Added general piece script

---
+++ Chess 2 Total War/Assets/_Scripts/Piece.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public enum PieceType
+{
+    Pawn,
+    Knight,
+    Bishop,
+    Samurai
+}
+
+public class Piece : MonoBehaviour
+{
+   [SerializeField] private Sprite sprite;
+    [SerializeField] private Vector2 tilePosition;
+    [SerializeField] private Vector2 pieceSize;
+
+    [SerializeField] private PieceType type;
+
+    public Vector2 GetTilePosition()
+    {
+        return tilePosition;
+    }
+
+    public PieceType GetPieceType()
+    {
+        return type;
+    }
+}
