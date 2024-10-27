7fdeb58d387e0ba60fbe4d03aad8d89ac5bde67b
Aidan Heathfield <aidan.heathfield@gmail.com>
2024-10-27 00:48:34 -0400

    trying to fix weird error

--- Chess 2 Total War/Assets/_Scripts/Tile.cs
+++ Chess 2 Total War/Assets/_Scripts/Tile.cs
@@ -5,15 +5,34 @@ public class Tile : MonoBehaviour
     [SerializeField] private Color _baseColor, _offsetColor;
     [SerializeField] private SpriteRenderer _renderer;
     [SerializeField] private GameObject _highlight;
-    public void Init(bool isOffset) {
+    [SerializeField] private int _col;
+    [SerializeField] private int _row;
+    
+
+    public void Init(bool isOffset, int col, int row) 
+    {
         _renderer.color = isOffset ? _offsetColor : _baseColor;
+        _col = col;
+        _row = row;
     }
 
-    void OnMouseEnter() {
+    public int GetTileColumn() 
+    {
+        return _col;
+    }
+
+    public int GetTileRow() 
+    {
+        return _row;
+    }
+
+    void OnMouseEnter() 
+    {
         _highlight.SetActive(true);
     }
 
-    void OnMouseExit() {
+    void OnMouseExit() 
+    {
         _highlight.SetActive(false);
     }
 }
