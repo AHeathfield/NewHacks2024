54a71833e166ef77c5333223982a904c2073a822
Aidan Heathfield <aidan.heathfield@gmail.com>
2024-10-27 03:29:41 -0400

    setted up pawns starting

--- Chess 2 Total War/Assets/_Scripts/GridManager.cs
+++ Chess 2 Total War/Assets/_Scripts/GridManager.cs
@@ -7,23 +7,25 @@ public class GridManager : MonoBehaviour
 {
     [SerializeField] private int _rows, _cols;
     [SerializeField] private Tile _tilePrefab;
-    [SerializeField] private Piece _piecePrefab;
+    [SerializeField] private GameObject[] _whitePawns = new GameObject[8];
+    [SerializeField] private GameObject[] _blackPawns = new GameObject[8];
     [SerializeField] private Transform _cam;
 
 
-    private Dictionary<Tile, Vector2> _tiles; //Tile can get us [r,c], Vector2 is where it actually is on screen
-    private Dictionary<Tile, Piece> _pieces;
+    // private Dictionary<Tile, Vector3> _tiles; //Tile can get us [r,c], Vector2 is where it actually is on screen
+    // private Dictionary<Tile, Piece> _pieces;
 
     void Start()
     {
         GenerateGrid();
+        SetPiecesUp();
     }
 
     void GenerateGrid() 
     {
         float tile_size = 1.35f;
 
-        _tiles = new Dictionary<Tile, Vector2>();
+        // _tiles = new Dictionary<Tile, Vector3>();
         for (int x = 0; x < _cols; x++) {
             for (int y = 0; y < _rows; y++) {
                 float x_pos = x * tile_size;
@@ -35,14 +37,39 @@ public class GridManager : MonoBehaviour
 
                 spawnedTile.Init(isOffset, x, y);
                 
-                _tiles[spawnedTile] = new Vector2(x, y);
+                // _tiles[spawnedTile] = new Vector3(x, y);
             }
         }
     }
 
+    private Vector3 convertRowColToVector(int col, int row)
+    {
+        return new Vector3(col*1.35f, row*1.35f);
+    }
+
     void SetPiecesUp() {
-        _pieces = new Dictionary<Tile, Piece>();
-        // var pawn1 = Instantiate(_piecePrefab, )
+        // White Pawns
+        for (int i = 0; i < _whitePawns.Length; i++) {
+            MovePiece(_whitePawns[i], convertRowColToVector(i,1));
+        }
+
+        // Black Pawns
+        for (int i = 0; i < _blackPawns.Length; i++) {
+            MovePiece(_blackPawns[i], convertRowColToVector(i,6));
+        }
+    }
+
+     private void MovePiece(GameObject piece, Vector3 newPosition)
+    {
+        if (piece != null)
+        {
+            piece.transform.position = newPosition;
+            Debug.Log($"Pawn moved to position: {newPosition}");
+        }
+        else
+        {
+            Debug.LogError("Pawn GameObject reference is missing!");
+        }
     }
 
     
