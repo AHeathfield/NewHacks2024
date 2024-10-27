ab00b730b8851096bef5dbf0468fad753055e113
Aidan Heathfield <aidan.heathfield@gmail.com>
2024-10-27 04:32:41 -0400

    Moved other pieces

--- Chess 2 Total War/Assets/_Scripts/GridManager.cs
+++ Chess 2 Total War/Assets/_Scripts/GridManager.cs
@@ -7,7 +7,14 @@ public class GridManager : MonoBehaviour
 {
     [SerializeField] private int _rows, _cols;
     [SerializeField] private Tile _tilePrefab;
+    [SerializeField] private GameObject whiteKing;
+    [SerializeField] private GameObject whiteQueen;
+    [SerializeField] private GameObject whiteKnight;
+    [SerializeField] private GameObject whiteRook1;
+    [SerializeField] private GameObject whiteRook2;
+    [SerializeField] private GameObject whiteRook3;
     [SerializeField] private GameObject whiteBishop1;
+    [SerializeField] private GameObject whiteBishop2;
     [SerializeField] private GameObject[] _whitePawns = new GameObject[8];
     [SerializeField] private GameObject[] _blackPawns = new GameObject[8];
     [SerializeField] private Transform _cam;
@@ -53,6 +60,16 @@ public class GridManager : MonoBehaviour
         for (int i = 0; i < _whitePawns.Length; i++) {
             MovePiece(_whitePawns[i], convertRowColToVector(i,1));
         }
+
+        // White pieces
+        MovePiece(whiteBishop1, convertRowColToVector(1,0));
+        MovePiece(whiteBishop2, convertRowColToVector(5,0));
+        MovePiece(whiteKnight, convertRowColToVector(0,0));
+        MovePiece(whiteRook1, convertRowColToVector(2,0));
+        MovePiece(whiteRook2, convertRowColToVector(6,0));
+        MovePiece(whiteRook3, convertRowColToVector(7,0));
+        MovePiece(whiteQueen, convertRowColToVector(3,0));
+        MovePiece(whiteKing, convertRowColToVector(4,0));
 
         // Black Pawns
         for (int i = 0; i < _blackPawns.Length; i++) {
