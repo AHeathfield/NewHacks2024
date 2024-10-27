fcbdfd035fe2f7c752c9000b9643abcb486dd58c
Aidan Heathfield <aidan.heathfield@gmail.com>
2024-10-26 22:05:51 -0400

    board size is now proper

--- Chess 2 Total War/Assets/_Scripts/GridManager.cs
+++ Chess 2 Total War/Assets/_Scripts/GridManager.cs
@@ -17,21 +17,23 @@ public class GridManager : MonoBehaviour
     }
 
     void GenerateGrid() {
+        float tile_size = 1.35f;
+
         _tiles = new Dictionary<Vector2, Tile>();
         for (int x = 0; x < _cols; x++) {
             for (int y = 0; y < _rows; y++) {
-                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
+                float x_pos = x * tile_size;
+                float y_pos = y * tile_size;
+
+                bool isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x_pos, y_pos), Quaternion.identity);
                 spawnedTile.name = $"Tile {x} {y}";
 
-                var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
                 spawnedTile.Init(isOffset);
                 
-
                 _tiles[new Vector2(x, y)] = spawnedTile;
             }
         }
-
-        _cam.transform.position = new Vector3((float)_rows/2 -0.5f, (float)_cols / 2 - 0.5f,-10);
     }
 
     public Tile GetTileAtPosition(Vector2 pos) {
