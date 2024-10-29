720a94f9fc3960d18b4f3928b75055350193d417
shamilemir <shamilcanbolat@gmail.com>
2024-10-29 17:38:03 -0400

    reorganized folders, quit no longer prints quit

--- Chess 2 Total War/Assets/_Scripts/MainMenu.cs
+++ Chess 2 Total War/Assets/_Scripts/MainMenu.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
+
 {
+    public Animator animator;
     public void PlayGame()
     {
+        animator.SetTrigger("PlayTrigger");
+        //StartCoroutine(DelayFunction());
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
     {
-        Debug.Log("QUIT!");
+        animator.SetTrigger("QuitTrigger");
         Application.Quit();
     }
 }
